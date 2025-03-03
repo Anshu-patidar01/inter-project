af8ecaa94caa125cb5f02dc179fdc24d746cfd36
Anshu Patidar <anshupatidar62@gmail.com>
2025-03-03 22:48:03 +0530

    footer

--- frontend/src/page/Footer.jsx
+++ frontend/src/page/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className="p-2 bg-gray-800">
-      <div className=" h-10 sm:items-center sm:p-10 p-3 text-white flex  flex-col sm:flex-row sm:flex sm:justify-between">
+      <div className=" h-10 sm:items-center sm:p-10 p-3 text-white flex  flex-col sm:flex-row sm:flex sm:justify-center">
         <div className="flex items-center justify-center gap-4 flex-row">
           <Link to={"/"}>Home</Link>
           <Link to={"/services"}>Service</Link>
@@ -12,7 +12,6 @@ function Footer() {
           <Link to={"/contact"}>Contact</Link>
           <Link to={"/"}>About</Link>
         </div>
-        <div></div>
       </div>
       <div className=" text-white text-center mt-5 sm:mt-0">
         Copyright @2025
